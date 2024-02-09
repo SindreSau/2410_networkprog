a0c6c49fa322890403c452eae16b32e09cb77917
SindreSau <101213882+SindreSau@users.noreply.github.com>
2024-02-09 16:11:46 +0100

    multithreaded server-client using sockets

---
+++ socket-programming-example/client.py
@@ -0,0 +1,25 @@
+import socket
+
+HEADER = 64
+PORT = 8080
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "10.0.0.1"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# ==(bind on server, connect on client) ==
+client.connect((SERVER, PORT)) # Connect the client to the server using a tuple
+
+def send(msg):
+    message = msg.encode(FORMAT) # Encode the message to bytes
+    msg_length = len(message) # Get the length of the message
+    send_length = str(msg_length).encode(FORMAT) # Encode the message length to bytes
+    send_length += b" " * (HEADER - len(send_length)) # b" " is saying that we want to send a byte string with a space, and we want to send it HEADER - len(send_length) times
+    client.send(send_length) # Send the message length
+    client.send(message) # Send the message
+    print(client.recv(2048).decode(FORMAT)) # Receive the message from the server 2048 because it's the buffer size
+
+
+if __name__ == "__main__":
+    send("Hello World!")
+    send(DISCONNECT_MESSAGE) # Send the disconnect message so the server knows to disconnect the client
--- socket-programming-example/server.py
+++ socket-programming-example/server.py
@@ -1,25 +1,47 @@
 import socket
 import threading
 
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
+HEADER = 64
+PORT = 8080
+SERVER = socket.gethostbyname(socket.gethostname()) # Get the server's IP address
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((SERVER, PORT)) # Bind the server to the port and the server's IP address using a tuple
 
 def handle_client(conn, addr):
-    pass
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT) # Receive the message length from the client
+        if msg_length:
+            msg_length = int(msg_length) # Convert the message length from string to integer
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            
+            print(f"[{addr}] {msg}")
+            if connected:
+                conn.send("Message received".encode(FORMAT))
+            else:
+                conn.send("Disconnecting".encode(FORMAT))
+    
+    conn.close()
 
 
 def start():
     server.listen()
-
+    print(f"[LISTENING] Server is listening on {SERVER}")
     # When a new connection occurs, server will accept the connection and
     while True:
-        conn, addr = server.connect()
+        conn, addr = server.accept() # conn is the connection object, addr is the address of the client
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}") # -1 because the start() function is also a thread
 
-print("[STARTING] Server is starting...")
-print(f"[LISTENING] Server is listening on {SERVER}")
-start()
+
+if __name__ == "__main__":
+    print("[STARTING] Server is starting...")
+    start()
